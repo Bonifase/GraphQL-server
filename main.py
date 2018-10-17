cc28a089ea278a5f9e28e06f3524cb101fc86175
Bonifase Orwa <orwabonifase@gmail.com>
2018-10-17 17:11:53 +0300

    initial setup

---
+++ main.py
@@ -0,0 +1,17 @@
+import graphene
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.String(default_value="stranger"))
+
+    def resolve_hello(self, info, name):
+        return 'Hello ' + name
+
+    def main():
+        schema = graphene.Schema(query=Query)
+        result = schema.execute({'hello'})
+        print(result.data['hello'])
+
+
+if __name__ == '__main__':
+    main()
