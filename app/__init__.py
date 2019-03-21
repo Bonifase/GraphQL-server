5f35812522ce903fd6b8fd9d694331b8febe00ef
Bonifase <orwabonifase@gmail.com>
2019-03-21 17:33:19 +0300

    set up graphql

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,42 @@
 from config import Config
+
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_graphql import GraphQLView
-from app.schema import schema
+from flask_jwt_extended import JWTManager
+
 
 db = SQLAlchemy()
 
+
 def create_app():
-    # Allows you to get configurations
-    # from the config file with the exact environment
-    # that you require
-    app.config.from_object((get_environment_config()))
+    app = Flask(__name__)
+    app.config.from_object((set_environment_config()))
+
+    jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def my_expired_token_callback():
+        return jsonify({
+            'status': 401,
+            'sub_status': 42,
+            'msg': 'The token has expired'
+        }), 401
+
     db.init_app(app)
+
+    from app.models.User import User
+    from app.models.VerificationCode import VerificationCode
+
     @app.before_first_request
     def initialize_database():
-        """ Create all tables """
         db.create_all()
-    @app.teardown_appcontext
-    def shutdown_session(exception=None):
-        db.session.remove()
+
+    @app.route('/')
+    def hello_world():
+        return "Hello World"
+
+    from app.schema import schema
 
     app.add_url_rule(
         '/graphql',
@@ -29,11 +47,15 @@ def create_app():
         )
     )
 
-    
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        db.session.remove()
+
     return app
 
-def get_environment_config():
-    if Config.ENV == "PRODUCTION":
-        return "config.ProductionConfig"
-    elif Config.ENV == "DEVELOPMENT":
-        return "config.DevelopmentConfig"
+
+def set_environment_config():
+    if(Config.ENV == "PRODUCTION"):
+        return 'config.ProductionConfig'
+    elif (Config.ENV == "DEVELOPMENT"):
+        return 'config.DevelopmentConfig'
--- manage.py
+++ manage.py
@@ -1,9 +1,19 @@
+
 from flask_script import Server, Manager
-from app import create_app
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
+
 
 app = create_app()
-manager = Manager(app)
-manager.add_command("runserver", Server)
 
-if __name__ == "__main__":
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+server = Server(host="0.0.0.0", port=5000)
+
+manager.add_command('db', MigrateCommand)
+manager.add_command("runserver", server)
+
+
+if __name__ == '__main__':
     manager.run()
